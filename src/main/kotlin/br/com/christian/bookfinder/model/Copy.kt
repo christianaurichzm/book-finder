11f374e397b32660a24b646d1170b55255fad2b3
Christian Aurich <christian.aurichzm@gmail.com>
2021-03-24 22:05:05 -0300

    Add Copy entity

---
+++ src/main/kotlin/br/com/christian/bookfinder/model/Copy.kt
@@ -0,0 +1,29 @@
+package br.com.christian.bookfinder.model
+
+import lombok.Data
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Data
+@Entity
+data class Copy (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    val book: Book,
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    val store: Store,
+
+    @Column(nullable = false)
+    val price: Double
+)
