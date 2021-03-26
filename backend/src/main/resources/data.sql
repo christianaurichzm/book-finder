695c7fce34271dc5ae39bb46c55714df304b2b30
Christian Aurich <christian.aurichzm@gmail.com>
2021-03-26 08:25:01 -0300

    Update data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,26 @@
 INSERT INTO book (title) VALUES ('The Hobbit');
+INSERT INTO book (title) VALUES ('The Lord of the Rings');
+INSERT INTO book (title) VALUES ('Harry Potter');
+INSERT INTO book (title) VALUES ('The Drunkards Walk');
+INSERT INTO book (title) VALUES ('Java How to Program');
+INSERT INTO book (title) VALUES ('A Brief History of Time');
+INSERT INTO book (title) VALUES ('Fundamentals of Database Systems');
+INSERT INTO book (title) VALUES ('Introduction to Algorithms');
+INSERT INTO book (title) VALUES ('Clean Architecture');
+INSERT INTO book (title) VALUES ('Compilers: Principles, Techniques, and Tools');
+INSERT INTO book (title) VALUES ('Modern Operating Systems');
 
 INSERT INTO store (name) VALUES ('Book Store');
+INSERT INTO store (name) VALUES ('Amazon');
+INSERT INTO store (name) VALUES ('The Strand');
+INSERT INTO store (name) VALUES ('Atlantis Books');
 
 INSERT INTO copy (book_id, store_id, price) VALUES (1, 1, 20);
+INSERT INTO copy (book_id, store_id, price) VALUES (1, 1, 20);
+INSERT INTO copy (book_id, store_id, price) VALUES (1, 3, 16);
+INSERT INTO copy (book_id, store_id, price) VALUES (4, 2, 15);
+INSERT INTO copy (book_id, store_id, price) VALUES (11, 2, 50);
+INSERT INTO copy (book_id, store_id, price) VALUES (9, 4, 30);
+INSERT INTO copy (book_id, store_id, price) VALUES (5, 1, 40);
+INSERT INTO copy (book_id, store_id, price) VALUES (3, 3, 15);
+INSERT INTO copy (book_id, store_id, price) VALUES (2, 4, 22);
